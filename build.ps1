2ee7677edb15b2a5fab83a6aba7a754048c0345c
budul100 <MarekGrochowski@web.de>
2023-10-12 01:06:06 +0200

    Build scripts added

---
+++ build.ps1
@@ -0,0 +1,1 @@
+pkg -t node18-win-x64 -o ./deployment/TransitMap.exe cli.js
--- debug.ps1
+++ debug.ps1
@@ -1,1 +1,2 @@
+Set-Item -Path Env:PATH -Value ($Env:PATH + ";" + (Get-Item .).FullName)
 cat examples\bvg.input.json | node.exe cli.js -y > examples\_output.svg
